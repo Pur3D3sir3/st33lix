1e47af5a0c78effe7544a94d9a227484690d8016
MasihTak <masihtak@outlook.com>
2022-07-14 20:23:34 +0200

    feat: add AppLoading component

---
+++ src/components/UI/AppLoading.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue";
+
+const show_loader = ref(true);
+
+setTimeout(() => {
+  show_loader.value = false;
+}, 3000);
+</script>
+
+<template>
+  <div
+    class="loader simple-loader animate__animated"
+    :class="{ animate__fadeOut: !show_loader }"
+    v-if="show_loader"
+  >
+    <div class="loader-body"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.simple-loader {
+    background-color: #141414;
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    place-content: center;
+    overflow: hidden !important;
+    right: 0;
+    z-index: 1250;
+}
+.loader-body {
+    background: url(../../assets/loader.gif) no-repeat scroll center center;
+    background-size: 20%;
+    width: 100%;
+    height: 100%;
+    position: relative;
+}
+
+</style>
